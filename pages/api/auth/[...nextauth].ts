0d51a7939f6098a58223217449570e87b73dafe8
Yangx <641160771@qq.com>
2025-06-12 15:46:52 +0800

    update Twitter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,9 +2,16 @@ import NextAuth from 'next-auth'
 import TwitterProvider from 'next-auth/providers/twitter'
 
 interface TwitterProfile {
-  id: string
-  username: string
-  name: string
+  data?: {
+    id: string
+    username: string
+    name: string
+    profile_image_url?: string
+  }
+  // Twitter API v2 直接返回的格式
+  id?: string
+  username?: string
+  name?: string
   profile_image_url?: string
 }
 
@@ -18,24 +25,64 @@ export default NextAuth({
   ],
   callbacks: {
     async jwt({ token, account, profile }) {
-      // 保存Twitter信息到token
+      // 调试信息
+      console.log('JWT Callback - Account:', account ? 'Present' : 'None');
+      console.log('JWT Callback - Profile:', profile ? 'Present' : 'None');
+      
       if (account) {
+        console.log('Account provider:', account.provider);
+        console.log('Account type:', account.type);
         token.accessToken = account.access_token
         token.refreshToken = account.refresh_token
       }
+      
       if (profile) {
+        console.log('Profile data:', JSON.stringify(profile, null, 2));
         const twitterProfile = profile as TwitterProfile
-        token.twitterUsername = twitterProfile.username
-        token.twitterId = twitterProfile.id
+        
+        // Twitter API v2 可能返回两种格式
+        const username = twitterProfile.data?.username || twitterProfile.username
+        const id = twitterProfile.data?.id || twitterProfile.id
+        
+        token.twitterUsername = username
+        token.twitterId = id
+        
+        console.log('Extracted username:', username);
+        console.log('Extracted id:', id);
+        console.log('Profile structure check:', {
+          hasData: !!twitterProfile.data,
+          hasDirectUsername: !!twitterProfile.username,
+          hasDirectId: !!twitterProfile.id
+        });
       }
+      
+      console.log('Final token:', { 
+        hasAccessToken: !!token.accessToken,
+        hasUsername: !!token.twitterUsername,
+        hasId: !!token.twitterId 
+      });
+      
       return token
     },
     async session({ session, token }) {
+      console.log('Session Callback - Token:', {
+        hasAccessToken: !!token.accessToken,
+        hasUsername: !!token.twitterUsername,
+        hasId: !!token.twitterId
+      });
+      
       // 发送Twitter信息到客户端
       session.accessToken = token.accessToken as string
       session.refreshToken = token.refreshToken as string
       session.twitterUsername = token.twitterUsername as string
       session.twitterId = token.twitterId as string
+      
+      console.log('Final session:', {
+        hasAccessToken: !!session.accessToken,
+        hasUsername: !!session.twitterUsername,
+        hasId: !!session.twitterId
+      });
+      
       return session
     },
     async redirect({ url, baseUrl }) {
