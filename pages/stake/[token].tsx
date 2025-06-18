3511711dfb2686964051b43cff4219de02bb0d57
Yangx <641160771@qq.com>
2025-06-18 16:16:51 +0800

    update stake page

--- pages/stake/[token].tsx
+++ pages/stake/[token].tsx
@@ -2,10 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { Button, Modal, Empty, Spin, App, Popover, Collapse, Input } from 'antd';
 import { LeftOutlined, ExportOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
+import { getContractConfig } from '@/wallet';
+import { useAppKitAccount, useAppKitProvider, useAppKitNetwork } from '@reown/appkit/react';
+import { useAppKitConnection } from '@reown/appkit-adapter-solana/react';
+import type { Provider } from '@reown/appkit-adapter-solana/react';
+import { usePageContext } from '@/context';
 
 export default function Stake() {
   const router = useRouter();
   const { token: token } = router.query;
+
+  const { address, isConnected } = useAppKitAccount();
+  const { caipNetwork, chainId } = useAppKitNetwork();
+  const { connection } = useAppKitConnection();
+  const { walletProvider } = useAppKitProvider<Provider>('solana');
 
   const [projectData, setProjectData] = useState([
     { rank: 1, avatar: '/assets/images/avatar-1.png', name: 'Aimonica' },
@@ -16,11 +26,16 @@ export default function Stake() {
   const [projectInfo, setProjectInfo] = useState<any>({});
   const [loading, setLoading] = useState(false);
   const [amount, setAmount] = useState('');
-  const [balance, setBalance] = useState(0);
-  const [tabIndex, setTabIndex] = useState(0);
-  const [isUnstakeModalOpen, setIsUnstakeModalOpen] = useState(false);
-  const [isEmergencyUnstakeModalOpen, setIsEmergencyUnstakeModalOpen] = useState(false);
   const [isStakeModalOpen, setIsStakeModalOpen] = useState(false);
+  const [days, setDays] = useState(7);
+  const [expectedPoints, setExpectedPoints] = useState(0);
+  const [tokenBalance, setTokenBalance] = useState(0);
+  const [tokenPrice, setTokenPrice] = useState(0);
+  const [tokenWorth, setTokenWorth] = useState(0);
+  const [totalUser, setTotalUser] = useState(0);
+  const [totalTVL, setTotalTVL] = useState(0);
+  const [poolAddress, setPoolAddress] = useState('');
+  const [poolLink, setPoolLink] = useState('');
 
   useEffect(() => {
     if (token) {
@@ -28,20 +43,29 @@ export default function Stake() {
     }
   }, [token]);
 
-  const handleTabChange = (index: number) => {
-    setTabIndex(index);
-  };
+  useEffect(() => {
+    if (isConnected && address && caipNetwork && chainId) {
+      const contractConfig = getContractConfig(chainId);
+      if (caipNetwork.chainNamespace === 'eip155') {
+        setPoolAddress(contractConfig.AimStaking);
+        const link = `${caipNetwork.blockExplorers.default.url}/address/${contractConfig.AimStaking}`;
+        setPoolLink(link);
+      } else if (caipNetwork.chainNamespace === 'solana') {
+        if (connection && walletProvider) {
+          setPoolAddress(contractConfig.programId);
+          const link = `${caipNetwork.blockExplorers.default.url}/account/${contractConfig.programId}?cluster=${contractConfig.cluster}`;
+          setPoolLink(link);
+        }
+      }
+    }
+  }, [isConnected, address, caipNetwork, chainId, connection, walletProvider]);
+
+  useEffect(() => {
+    setExpectedPoints(Number(amount));
+  }, [amount]);
 
   const handleStake = () => {
     setIsStakeModalOpen(true);
-  };
-
-  const handleEmergencyUnstake = () => {
-    setIsEmergencyUnstakeModalOpen(true);
-  };
-
-  const handleUnstake = () => {
-    setIsUnstakeModalOpen(true);
   };
 
   return (
@@ -86,214 +110,84 @@ export default function Stake() {
               <div className="text2">
                 <div>
                   <span>Users</span>
-                  <span>1000</span>
+                  <span>{totalUser}</span>
                 </div>
                 <div>
                   <span>TVL</span>
-                  <span>$ 1000</span>
+                  <span>$ {totalTVL}</span>
                 </div>
               </div>
               <div className="text3">
                 <span>Pool Address</span>
-                <a className="pool-address">
-                  8B4j....KTY5s
+                <a className="pool-address" href={poolLink} target="_blank">
+                  {poolAddress.slice(0, 6)}...{poolAddress.slice(-4)}
                   <ExportOutlined />
                 </a>
               </div>
             </div>
           </div>
           <div className="right-box">
-            <div className="tab-title-box">
-              <button className={tabIndex === 0 ? 'active' : ''} onClick={() => handleTabChange(0)}>
-                STAKE
-              </button>
-              <button className={tabIndex === 1 ? 'active' : ''} onClick={() => handleTabChange(1)}>
-                UNSTAKE
-              </button>
-            </div>
-            {tabIndex === 0 ? (
-              <div className="stake-item">
-                <div className="text">
-                  <span> Rewards </span>
-                  <div className="s-box">
-                    <div className="s-img">
-                      <img src="/assets/images/img-5.png" alt="" />
-                    </div>
-                    <div className="s-text">Points 7.5x AIM</div>
+            <div className="tab-title-box">STAKE</div>
+            <div className="stake-item">
+              <div className="text">
+                <span> Rewards </span>
+                <div className="s-box">
+                  <div className="s-img">
+                    <img src="/assets/images/img-5.png" alt="" />
                   </div>
+                  <div className="s-text">Points 7.5x AIM</div>
                 </div>
-                <div className="text margin-bottom">
-                  <span>Your Balance</span>
+              </div>
+              <div className="text margin-bottom">
+                <span>Your Balance</span>
+              </div>
+              <div className="avatar-box-box">
+                <div className="avatar-box">
+                  <img src="/assets/images/avatar.png" alt="" />
+                  <span>Aimonica</span>
                 </div>
-                <div className="avatar-box-box">
-                  <div className="avatar-box">
-                    <img src="/assets/images/avatar.png" alt="" />
-                    <span>Aimonica</span>
-                  </div>
-                  <div className="number-box">
-                    <div className="number">1500</div>
-                    <div className="number2">$ 15</div>
-                  </div>
+                <div className="number-box">
+                  <div className="number">{tokenBalance}</div>
+                  <div className="number2">$ {tokenWorth}</div>
                 </div>
-                <div className="inputbox">
-                  <Input
-                    type="number"
-                    placeholder={`Enter Amount`}
-                    value={amount}
-                    onChange={(e) => setAmount(e.target.value)}
-                    disabled={loading}
-                  />
-                  <button className="max-btn" onClick={() => setAmount(balance.toString())}>
-                    MAX
+              </div>
+              <div className="inputbox">
+                <Input
+                  type="number"
+                  placeholder={`Enter Amount`}
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  disabled={loading}
+                />
+                <button className="max-btn" onClick={() => setAmount(tokenBalance.toString())}>
+                  MAX
+                </button>
+              </div>
+              <div className="text">
+                <span>Locking Time</span>
+                <div className="days">
+                  <button className={days === 7 ? 'active' : ''} onClick={() => setDays(7)}>
+                    7D
+                  </button>
+                  <button className={days === 14 ? 'active' : ''} onClick={() => setDays(14)}>
+                    14D
+                  </button>
+                  <button className={days === 30 ? 'active' : ''} onClick={() => setDays(30)}>
+                    30D
                   </button>
                 </div>
-                <div className="text">
-                  <span>Locking Time</span>
-                  <div className="days">
-                    <button>7D</button>
-                    <button>14D</button>
-                    <button>30D</button>
-                  </div>
-                </div>
-                <div className="text">
-                  <span>Expected Points</span>
-                  <div className="number">1500</div>
-                </div>
-                <Button type="primary" size="large" className="stake-btn" onClick={handleStake} loading={loading}>
-                  STAKE
-                </Button>
               </div>
-            ) : (
-              <div className="stake-item">
-                <div className="text">
-                  <span>Your Balance</span>
-                </div>
-                <div className="avatar-box-box margin-bottom">
-                  <div className="avatar-box">
-                    <img src="/assets/images/avatar.png" alt="" />
-                    <span>Aimonica</span>
-                  </div>
-                  <div className="number-box">
-                    <div className="number">1500</div>
-                  </div>
-                </div>
-                <div className="text">
-                  <span>Your Earned Points</span>
-                  <div className="s-box">
-                    <div className="s-img">
-                      <img src="/assets/images/img-3.png" alt="" />
-                    </div>
-                    <div className="s-text">10000</div>
-                  </div>
-                </div>
-                <div className="text">
-                  <span>Staking start time</span>
-                  <span>2025-5-25 17:26:46</span>
-                </div>
-                <div className="text">
-                  <span>Locking Time</span>
-                  <span>14D:12H:50M:24S</span>
-                </div>
-                <div className="text">
-                  <span>Redemption Time</span>
-                  <span>2025-5-25 17:26:46</span>
-                </div>
-                <div className="text">
-                  <span>Unstaking Fee</span>
-                  <span className="num">5%</span>
-                </div>
-                <Button
-                  type="primary"
-                  size="large"
-                  className="stake-btn"
-                  onClick={handleEmergencyUnstake}
-                  loading={loading}>
-                  Emergency Unstake
-                </Button>
-                <div className="text2">(*Warning: Unstaking in advance will result in a 30% deduction of rewards*)</div>
-                <Button type="primary" size="large" className="stake-btn" onClick={handleUnstake} loading={loading}>
-                  Unstake
-                </Button>
+              <div className="text">
+                <span>Expected Points</span>
+                <div className="number">{expectedPoints}</div>
               </div>
-            )}
+              <Button type="primary" size="large" className="stake-btn" onClick={handleStake} loading={loading}>
+                STAKE
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-
-      <Modal
-        className="unstake-modal"
-        width={'fit-content'}
-        centered
-        closable={false}
-        footer={null}
-        open={isEmergencyUnstakeModalOpen}
-        onOk={() => setIsEmergencyUnstakeModalOpen(false)}
-        onCancel={() => setIsEmergencyUnstakeModalOpen(false)}>
-        <div className="unstake-modal-box">
-          <img src="/assets/images/img-26.png" alt="" className="img-26" />
-          <div className="title">Emergency Unstake</div>
-          <div className="text">（*Warning: Unstaking in advance will result in a 30% deduction of rewards*）</div>
-          <div className="text2">
-            <div className="text2-1">You can only get</div>
-            <div className="text2-2">
-              <div>15K Aimonica(≈$1,500)</div>
-              <div className="s-box">
-                <div className="s-img">
-                  <img src="/assets/images/img-3.png" alt="" />
-                </div>
-                <div className="s-text">1321546521</div>
-              </div>
-            </div>
-          </div>
-          <div className="btn-box">
-            <button className="btn-cancel" onClick={() => setIsEmergencyUnstakeModalOpen(false)}>
-              Cancel
-            </button>
-            <button className="btn-confirm" onClick={() => setIsEmergencyUnstakeModalOpen(false)}>
-              Confirm
-            </button>
-          </div>
-        </div>
-      </Modal>
-
-      <Modal
-        className="unstake-modal"
-        width={'fit-content'}
-        centered
-        closable={false}
-        footer={null}
-        open={isUnstakeModalOpen}
-        onOk={() => setIsUnstakeModalOpen(false)}
-        onCancel={() => setIsUnstakeModalOpen(false)}>
-        <div className="unstake-modal-box">
-          <img src="/assets/images/img-26.png" alt="" className="img-26" />
-          <div className="title">Unstake</div>
-          <div className="text-box2">
-            <div>Unstaking Fee</div>
-            <div>5%</div>
-          </div>
-          <div className="text2 text3">
-            <div className="text2-1">You can only get</div>
-            <div className="text2-2">
-              <div>15K Aimonica(≈$1,500)</div>
-              <div className="s-box">
-                <div className="s-img">
-                  <img src="/assets/images/img-3.png" alt="" />
-                </div>
-                <div className="s-text">1321546521</div>
-              </div>
-            </div>
-          </div>
-          <div className="btn-box">
-            <button className="btn-cancel" onClick={() => setIsUnstakeModalOpen(false)}>
-              Cancel
-            </button>
-            <button className="btn-confirm" onClick={() => setIsUnstakeModalOpen(false)}>
-              Confirm
-            </button>
-          </div>
-        </div>
-      </Modal>
 
       <Modal
         className="stake-modal"
